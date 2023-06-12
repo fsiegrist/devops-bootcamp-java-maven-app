a5d3fc5d2a4bfc73b8ad393feaec4976b0637784
Felix Siegrist <felix.siegrist@inventage.com>
2023-06-12 23:44:25 +0200

    Fix application port

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,8 +57,8 @@ resource "aws_default_security_group" "default-sg" {
     }
 
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = 8000
+        to_port = 8000
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
